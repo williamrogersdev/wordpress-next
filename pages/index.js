bd68a2d4b6161fb177113c354bcccdaccaeebd26
William Rogers <williamrogersdev@gmail.com>
2023-02-16 01:00:02 -0600

    added fetch to get posts from website

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,41 @@
-export default function Home() {
-  return (
-    <h1> Hello</h1>
+import Link from 'next/link'
+
+export default function Home( {posts} ){
+
+  return(
+    <div>
+      <h1>Hello From The Home Page!</h1>
+      
+    </div>
   )
+
 }
 
+export async function getStaticProps(){
 
-export async function getStaticProps() {
-  
+  const res = await fetch('https://buysellrecycle.com/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+          query: `
+          query HomePageQuery {
+            posts {
+              nodes {
+                slug
+                title
+              }
+            }
+          }
+          `,
+      })
+  })
+
+  const json = await res.json()
+
+  return {
+    props: {
+        posts: json.data.posts,
+    },
+  }
+
 }
