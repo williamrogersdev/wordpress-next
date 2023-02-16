8cca593fdc0c016960b8f5f3229ea5d9c492d5e5
William Rogers <williamrogersdev@gmail.com>
2023-02-16 01:01:09 -0600

    added posts

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,17 @@ export default function Home( {posts} ){
   return(
     <div>
       <h1>Hello From The Home Page!</h1>
-      
+      {
+        posts.nodes.map(post => {
+          return(
+            <ul key={post.slug}>
+              <li>
+                <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+              </li>
+            </ul>
+          )
+        })
+      }
     </div>
   )
 
